3be585f7a49e14b137fca9a2a455637b20a411f3
Denis Khrunin <zak63507@gmail.com>
2025-11-11 17:33:06 +0300

    new branch

--- grapes/src/timing.rs
+++ grapes/src/timing.rs
@@ -1,5 +1,9 @@
 use crate::RT;
-use std::time::Duration;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+    time::Duration,
+};
 use tokio::{task::JoinHandle, time::sleep};
 
 pub struct Interval {
@@ -45,5 +49,73 @@ impl Timeout {
 
     pub fn clear(self) {
         self.handle.abort();
+    }
+}
+
+impl Future for Timeout {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        if self.handle.is_finished() {
+            Poll::Ready(())
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        sync::{Arc, RwLock},
+        time::Duration,
+    };
+
+    #[tokio::test]
+    async fn timeout_set() {
+        tokio::time::pause();
+
+        let count = Arc::new(RwLock::new(0));
+        let duration = Duration::from_secs(1);
+
+        let count_clone = count.clone();
+        let timeout = Timeout::set(
+            move || {
+                let mut count = count_clone.write().unwrap();
+                *count += 1;
+            },
+            duration,
+        );
+
+        tokio::time::advance(duration).await;
+        timeout.await;
+
+        let result = *count.read().unwrap();
+        assert_eq!(result, 1);
+    }
+
+    #[tokio::test]
+    async fn timeout_cancel() {
+        tokio::time::pause();
+
+        let count = Arc::new(RwLock::new(0));
+        let duration = Duration::from_secs(1);
+
+        let count_clone = count.clone();
+        let timeout = Timeout::set(
+            move || {
+                let mut count = count_clone.write().unwrap();
+                *count += 1;
+            },
+            duration,
+        );
+
+        tokio::time::advance(duration / 2).await;
+        timeout.clear();
+        tokio::time::advance(duration / 2).await;
+
+        let result = *count.read().unwrap();
+        assert_eq!(result, 0);
     }
 }
