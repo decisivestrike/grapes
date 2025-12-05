706439433405c14029b623f642b4950aa5cd982d
inqlog <not@example.com>
2025-12-05 23:02:08 +0300

    add tests

--- grapes/src/lib.rs
+++ grapes/src/lib.rs
@@ -42,3 +42,40 @@ use std::sync::LazyLock;
 use tokio::runtime::Runtime;
 
 pub static RT: LazyLock<Runtime> = LazyLock::new(|| Runtime::new().unwrap());
+
+/// Run tests in single thread
+///
+/// `cargo test -- --test-threads=1`
+#[cfg(test)]
+mod tests {
+
+    use crate::prelude::*;
+    use gtk::gdk::Monitor;
+
+    static mut IS_INIT: bool = false;
+
+    fn gtk_safe_init() {
+        unsafe {
+            if !IS_INIT {
+                gtk::init().unwrap();
+                IS_INIT = true;
+            }
+        }
+    }
+
+    #[test]
+    fn test_monitors_all() {
+        gtk_safe_init();
+
+        let monitors = Monitor::all();
+        assert!(!monitors.is_empty());
+    }
+
+    #[test]
+    fn test_monitors_all2() {
+        gtk_safe_init();
+
+        let monitors = Monitor::all();
+        assert!(!monitors.is_empty());
+    }
+}
