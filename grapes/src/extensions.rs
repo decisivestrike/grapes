44fac296efed2d1e35bb7c1b04523381a9e1938e
inqlog <not@example.com>
2025-12-05 21:52:10 +0300

    add extension trait for gdkMonitor

--- grapes/src/extensions.rs
+++ grapes/src/extensions.rs
@@ -1,5 +1,9 @@
-use gtk::Widget;
-use gtk::prelude::{BoxExt, WidgetExt};
+use gtk::{
+    Widget,
+    gdk::{self, prelude::DisplayExt},
+    glib::object::Cast,
+    prelude::{BoxExt, WidgetExt},
+};
 use std::iter::successors;
 
 pub trait GrapesBoxExt {
@@ -15,5 +19,21 @@ impl GrapesBoxExt for gtk::Box {
 
     fn children(&self) -> impl Iterator<Item = Widget> {
         successors(self.first_child(), |child| child.next_sibling())
+    }
+}
+
+pub trait GrapesMonitorExt {
+    fn all() -> Vec<gdk::Monitor>;
+}
+
+impl GrapesMonitorExt for gdk::Monitor {
+    fn all() -> Vec<gdk::Monitor> {
+        let display = gdk::Display::default().expect("No display");
+
+        display
+            .monitors()
+            .into_iter()
+            .filter_map(|obj| obj.ok()?.downcast::<gdk::Monitor>().ok())
+            .collect()
     }
 }
