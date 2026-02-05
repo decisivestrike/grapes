49ee65ee9a28908099cfa8fc8c945e25defd803e
Denis <zak63507@gmail.com>
2026-02-05 10:30:41 +0300

    Remove Updateable impl for State

--- grapes/src/updateable.rs
+++ grapes/src/updateable.rs
@@ -1,15 +1,5 @@
-use crate::State;
-
 pub trait Updateable: 'static {
     type Message: Clone + 'static;
 
     fn update(&self, message: Self::Message);
-}
-
-impl<T: Clone + 'static> Updateable for State<T> {
-    type Message = T;
-
-    fn update(&self, message: T) {
-        self.set(message);
-    }
 }
