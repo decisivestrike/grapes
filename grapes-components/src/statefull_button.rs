37fc92d66b87610b6b310ca257968dc82bf103d1
Denis <zak63507@gmail.com>
2026-02-25 13:06:24 +0300

    add statefull button

---
+++ grapes-components/src/statefull_button.rs
@@ -0,0 +1,43 @@
+use grapes::{Component, State, effect, glib::clone::Downgrade, gtk};
+use std::rc::Rc;
+
+#[derive(Debug, Component)]
+pub struct StatefullButton<T>
+where
+    T: 'static,
+{
+    #[root]
+    label: gtk::Label,
+    state: Rc<State<T>>,
+}
+
+impl<T> StatefullButton<T>
+where
+    T: AsRef<str>,
+{
+    pub fn new(state: &Rc<State<T>>) -> StatefullButton<T> {
+        let label = gtk::Label::new(None);
+
+        {
+            let weak_state = state.downgrade();
+            let weak_label = label.downgrade();
+
+            effect(move || {
+                if let Some(state) = weak_state.upgrade()
+                    && let Some(label) = weak_label.upgrade()
+                {
+                    label.set_label(state.get().as_ref());
+                }
+            });
+        }
+
+        Self {
+            label,
+            state: state.clone(),
+        }
+    }
+
+    pub fn state(&self) -> Rc<State<T>> {
+        self.state.clone()
+    }
+}
