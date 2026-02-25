de23d5862f9dd4391cf6ee0fc27e2d5f3bee512a
Denis <zak63507@gmail.com>
2026-02-25 13:03:45 +0300

    fix: macro

--- grapes-macros/src/lib.rs
+++ grapes-macros/src/lib.rs
@@ -12,7 +12,8 @@ pub fn component(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = &input.ident;
 
-    let (impl_generics, ty_generics, _) = input.generics.split_for_impl();
+    let (impl_generics, ty_generics, where_clause) =
+        input.generics.split_for_impl();
 
     let mut maybe_root_ts: Option<TokenStream2> = None;
     let mut maybe_state_ts: Option<TokenStream2> = None;
@@ -33,9 +34,9 @@ pub fn component(input: TokenStream) -> TokenStream {
                     }
 
                     let expanded = quote! {
-                        impl #impl_generics Component for #struct_name #ty_generics {}
+                        impl #impl_generics Component for #struct_name #ty_generics #where_clause {}
 
-                        impl #impl_generics AsRef<::grapes::gtk::Widget> for #struct_name #ty_generics {
+                        impl #impl_generics AsRef<::grapes::gtk::Widget> for #struct_name #ty_generics #where_clause {
                             fn as_ref(&self) -> &::grapes::gtk::Widget {
                                 use ::grapes::gtk::prelude::Cast;
                                 self.#field_name.as_ref()
@@ -66,7 +67,7 @@ pub fn component(input: TokenStream) -> TokenStream {
                     };
 
                     let expanded = quote! {
-                        impl #impl_generics ::grapes::Updateable for #struct_name #ty_generics {
+                        impl #impl_generics ::grapes::Updateable for #struct_name #ty_generics #where_clause {
                             type Message = #generic_type;
 
                             fn update(&self, value: #generic_type) {
